f3bc46f450a6bfe75f1f486e56df3f9e2bddd3af
spappa3k <pappalardo.dev@gmail.com>
2025-01-28 18:24:25 +0100

    .

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Scansiona i file HTML e TypeScript nella directory src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
